3ff0c8d58c76f35ede17c8a32578fdd7972f0936
markongithub <markongithub@digamma.net>
2023-02-18 15:39:40 +0000

    make typescript happy, supposedly

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -5,6 +5,8 @@ type Post = {
   
   import { prisma } from "~/db.server";
 
+  import type { Post } from "@prisma/client";
+
   export async function getPosts() {
     return prisma.post.findMany();
   }
@@ -14,5 +16,6 @@ type Post = {
   }
   
   export async function createPost(post) {
+    post: Pick<Post, "slug" | "title" | "markdown">
     return prisma.post.create({ data: post });
   }  
